9575c58be771debdc3e5a09a0b5fc8e6059674b2
Kevin <akmtime@hotmail.com>
2023-10-24 10:09:08 +0200

    MAJ-1

--- update.php
+++ update.php
@@ -1,74 +1,68 @@
 <?php
-session_start(); // Démarre ou reprend une session existante.
+    session_start(); // Démarre ou reprend une session existante.
 
-// Inclure la configuration de la base de données
-include 'includes/dbh.inc.php';
+    // Inclure la configuration de la base de données
+    include 'includes/dbh.inc.php';
 
-if (isset($_POST['update_product'])) {
-    // Assurez-vous de vérifier si les valeurs POST sont définies avant de les utiliser.
-    if (isset($_POST['product_id'], $_POST['product_name'], $_POST['product_price'], $_POST['rubric_name'])) {
-        $product_id = $_POST['product_id'];
-        $product_name = $_POST['product_name'];
-        $product_price = $_POST['product_price'];
-        $rubric_name = $_POST['rubric_name'];
-
-        // Effectuer la logique de modification
-        $edit_sql = "UPDATE products SET name=?, price=?, category=? WHERE id=?";
-        $edit_stmt = mysqli_stmt_init($conn);
-
-        if (mysqli_stmt_prepare($edit_stmt, $edit_sql)) {
-            mysqli_stmt_bind_param($edit_stmt, "ssdi", $product_name, $product_price, $rubric_name, $product_id);
-            if (mysqli_stmt_execute($edit_stmt)) {
-                // La mise à jour a réussi, vous pouvez rediriger l'utilisateur
-                header("Location: edit-success.php");
-                exit();
-            } else {
-                // Gestion de l'erreur de mise à jour
-                echo "Erreur lors de la mise à jour du produit.";
-            }
-        }
-    } else {
-        // Les données POST nécessaires pour la mise à jour ne sont pas toutes présentes.
-        echo "Données manquantes pour la mise à jour.";
-    }
-}
-
-// Valider l'action (assurez-vous que c'est soit 'edit' soit 'delete')
-if (isset($_POST['action'])) {
-    $action = $_POST['action'];
-
-    if ($action === 'edit' || $action === 'delete') {
-        if (isset($_POST['product_id']) && is_numeric($_POST['product_id'])) {
+    if (isset($_POST['update_product'])) {
+        // Assurez-vous de vérifier si les valeurs POST sont définies avant de les utiliser.
+        if (isset($_POST['product_id'], $_POST['product_name'], $_POST['product_price'], $_POST['rubric_name'])) {
             $product_id = $_POST['product_id'];
+            $product_name = $_POST['product_name'];
+            $product_price = $_POST['product_price'];
+            $rubric_name = $_POST['rubric_name'];
 
-            // Échapper les données du formulaire (pour éviter les injections SQL)
-            $product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
-            $product_price = floatval($_POST['price']); // Convertir en nombre à virgule flottante
-            $rubric_name = mysqli_real_escape_string($conn, $_POST['rubric_name']);
-        }
+            // Effectuer la logique de modification
+            $edit_sql = "UPDATE products SET name=?, price=?, category=? WHERE id=?";
+            $edit_stmt = mysqli_stmt_init($conn);
 
-        if ($action === 'delete') {
-            // Effectuer la logique de suppression
-            $delete_sql = "DELETE FROM products WHERE id=?";
-            $delete_stmt = mysqli_stmt_init($conn);
-        
-            if (mysqli_stmt_prepare($delete_stmt, $delete_sql)) {
-                mysqli_stmt_bind_param($delete_stmt, "i", $product_id);
-                if (mysqli_stmt_execute($delete_stmt)) {
-                    // Le produit a été supprimé avec succès
-                    header("Location: delete-success.php");
+            if (mysqli_stmt_prepare($edit_stmt, $edit_sql)) {
+                mysqli_stmt_bind_param($edit_stmt, "ssdi", $product_name, $product_price, $rubric_name, $product_id);
+                if (mysqli_stmt_execute($edit_stmt)) {
+                    // La mise à jour a réussi, vous pouvez rediriger l'utilisateur
+                    header("Location: edit-success.php");
                     exit();
                 } else {
-                    // Gestion de l'erreur de suppression
-                    echo "Erreur lors de la suppression du produit.";
+                    // Gestion de l'erreur de mise à jour
+                    echo "Erreur lors de la mise à jour du produit.";
                 }
             }
         } else {
-            // Product ID non valide
-            echo "ID de produit non valide.";
+            // Les données POST nécessaires pour la mise à jour ne sont pas toutes présentes.
+            echo "Données manquantes pour la mise à jour.";
         }
     }
-}
+    
+    if (isset($_GET['id'])) {
+        $product_id = $_GET['id'];
+        $query = "SELECT name, price, category FROM products WHERE id = ?";
+        
+        // Préparez la requête
+        $stmt = mysqli_prepare($conn, $query);
+    
+        if ($stmt) {
+            // Liez les paramètres et exécutez la requête
+            mysqli_stmt_bind_param($stmt, "i", $product_id);
+            mysqli_stmt_execute($stmt);
+    
+            // Obtenir le résultat
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if ($row = mysqli_fetch_assoc($result)) {
+                $product_name = $row['name'];
+                $product_price = $row['price'];
+                $rubric_name = $row['category'];
+            } else {
+                // Aucun enregistrement trouvé avec l'ID spécifié
+                echo "Aucun produit trouvé avec l'ID spécifié.";
+            }
+        } else {
+            // Erreur de requête préparée
+            echo "Erreur de requête préparée : " . mysqli_error($conn);
+        }
+    }
+    
+
 ?>
 
 
@@ -80,51 +74,53 @@ if (isset($_POST['action'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Product</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Geologica&family=Inter&family=Paprika&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="style/add-product-style.css">
 </head>
 
 <body>
 
-    <?php
-    $page = 'update-product';
-    include('header.php');
-    
-    // Récupérez les valeurs product_id, product_name, product_price, et rubric_name depuis le code PHP.
-    // Assurez-vous que ces valeurs sont définies avant de les utiliser.
-    $product_id = isset($product_id) ? $product_id : '';
-    $product_name = isset($product_name) ? $product_name : '';
-    $product_price = isset($product_price) ? $product_price : '';
-    $rubric_name = isset($rubric_name) ? $rubric_name : '';
-    ?>
+    <?php $page = 'update-product';include('header.php'); ?>
 
     <section class="product-form">
         <form action="" method="POST" enctype="multipart/form-data">
-            <h3>Product Info</h3>
+            <h3>Update product</h3>
 
             <!-- Ajoutez des champs de formulaire pour product_id, product_name, product_price, et rubric_name -->
             <input type="text" name="product_id" value="<?php echo $product_id; ?>">
 
             <p>Product Name <span>*</span></p>
-            <input type="text" name="product_name" value="<?php echo $product_name; ?>" placeholder="Enter product name" required maxlength="50" class="box">
+            <input type="text" name="product_name" value="<?php echo $product_name;?>" placeholder="Enter product name" required maxlength="50" class="box">
 
             <p>Product Price <span>*</span></p>
             <input type="number" name="price" value="<?php echo $product_price; ?>" placeholder="Enter product price" required min="0" max="9999999999" maxlength="10" class="box">
 
             <p>Rubric <span>*</span></p>
             <select name="rubric_name" required class="box">
-                <option value="pizza" <?php if ($rubric_name == 'pizza') echo 'selected'; ?>>Pizza</option>
-                <option value="burger" <?php if ($rubric_name == 'burger') echo 'selected'; ?>>Burger</option>
-                <option value="tacos" <?php if ($rubric_name == 'tacos') echo 'selected'; ?>>Tacos</option>
-                <option value="wrap" <?php if ($rubric_name == 'wrap') echo 'selected'; ?>>Wrap</option>
-                <option value="fries" <?php if ($rubric_name == 'fries') echo 'selected'; ?>>Fries</option>
-                <option value="salad" <?php if ($rubric_name == 'salad') echo 'selected'; ?>>Salad</option>
-                <option value="drink" <?php if ($rubric_name == 'drink') echo 'selected'; ?>>Drink</option>
+                <option value="Pizza" <?php if ($rubric_name == 'Pizza') echo 'selected'; ?>>Pizza</option>
+                <option value="Burger" <?php if ($rubric_name == 'Burger') echo 'selected'; ?>>Burger</option>
+                <option value="Tacos" <?php if ($rubric_name == 'Tacos') echo 'selected'; ?>>Tacos</option>
+                <option value="Wrap" <?php if ($rubric_name == 'Wrap') echo 'selected'; ?>>Wrap</option>
+                <option value="Fries" <?php if ($rubric_name == 'Fries') echo 'selected'; ?>>Fries</option>
+                <option value="Salad" <?php if ($rubric_name == 'Salad') echo 'selected'; ?>>Salad</option>
+                <option value="Drink" <?php if ($rubric_name == 'Drink') echo 'selected'; ?>>Drink</option>
             </select>
 
             <!-- Assurez-vous d'ajouter le bouton de soumission pour la mise à jour du produit -->
             <input type="submit" class="btn" name="update_product" value="Update Product">
         </form>
     </section>
+
+    <?php
+    
+    echo "Product ID: $product_id<br>";
+    echo "Product Name: $product_name<br>";
+    echo "Product Price: $product_price<br>";
+    echo "Rubric Name: $rubric_name<br>";
+    
+    ?>
 
 </body>
 
